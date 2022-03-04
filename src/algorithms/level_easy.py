8ce8807156dbc2c4d88d2b9d11b5c880ac3c6cdf
musubr <murali.subramanian@vortexa.com>
2022-03-04 20:12:05 +0000

    challenge: Solve and test palindrome problem wo converting int to str

--- src/algorithms/level_easy.py
+++ src/algorithms/level_easy.py
@@ -1,7 +1,10 @@
+import math
+
 # ------------------------------------------------------------------------------------------------------------------------------------------
 # Problem 1
 # Given an integer x, return true if x is palindrome integer. An integer is a palindrome when it reads the same backward as forward.
 # For example, 121 is a palindrome while 123 is not.
+
 
 def is_palindrome(num: int) -> bool:
     """Returns true if the input number is a palindrome.
@@ -11,4 +14,40 @@ def is_palindrome(num: int) -> bool:
     """
     return num == int(str(num)[::-1])
 
+
+def check_if_palindrome_wo_int_to_str_conversion(num: int) -> bool:
+    """Returns true if the input number is a palindrome.
+
+    This function attempts to solve the same problem without converting the input integer to a string.
+
+    Args:
+        num: input number
+    """
+    order = 1
+    mod_result = 0
+    reverse_number = 0
+    reverse_num_list = []
+
+    if (num >= 0) and (num < 10):
+        return True
+    elif num < 0:
+        return False
+    else:
+        while mod_result != num:
+            mod_check = 10**order
+            previous_mod_check = 10**(order-1)
+            mod_result = num % mod_check
+
+            if mod_result == (num % previous_mod_check):
+                mod_result = 0
+            if (mod_result == 0) and (order == 1):
+                return False
+
+            reverse_num_list += [math.floor(mod_result/previous_mod_check)]
+            order += 1
+
+        for i in range(len(reverse_num_list)):
+            reverse_number += reverse_num_list[i]*(10**(len(reverse_num_list)-1-i))
+
+        return num == reverse_number
 # ------------------------------------------------------------------------------------------------------------------------------------------
--- tests/algorithms/test_level_easy.py
+++ tests/algorithms/test_level_easy.py
@@ -1,5 +1,5 @@
 import pytest
-from src.algorithms.level_easy import is_palindrome
+from src.algorithms.level_easy import is_palindrome, check_if_palindrome_wo_int_to_str_conversion
 
 
 class TestLevelEasy:
@@ -16,6 +16,23 @@ class TestLevelEasy:
 
         # When
         output = is_palindrome(input_number)
+
+        # Then
+        assert output == expected_output
+
+    @pytest.mark.unit
+    @pytest.mark.parametrize(
+        "input_number, expected_output",
+        [
+            (123321, True),
+            (100011, False)
+        ]
+    )
+    def test__check_if_palindrome_wo_int_to_str_conversion_returns_true_if_input_integer_is_a_palindrome(self, input_number, expected_output):
+        # Given input number
+
+        # When
+        output = check_if_palindrome_wo_int_to_str_conversion(input_number)
 
         # Then
         assert output == expected_output
